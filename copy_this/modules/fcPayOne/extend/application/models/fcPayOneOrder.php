b3f0d828ebcac1122070f5e8c18c7373be06d90f
Stefan Müller <stefan.mueller@fatchip.de>
2019-04-15 15:01:41 +0200

    OXID-245: fixed ratepay userid is saved instead of shopid

--- copy_this/modules/fcPayOne/extend/application/models/fcPayOneOrder.php
+++ copy_this/modules/fcPayOne/extend/application/models/fcPayOneOrder.php
@@ -1641,10 +1641,11 @@ class fcPayOneOrder extends fcPayOneOrder_parent {
      */
     protected function _fcpoSaveProfileIdent($sPaymentId, $aResponse) {
         if (in_array($sPaymentId, $this->_aPaymentsProfileIdentSave)) {
-            $sProfileIdent = (isset($aResponse['userid'])) ? $aResponse['userid'] : false;
-            if ($sProfileIdent) {
-                $this->oxorder__fcpoprofileident = new oxField($sProfileIdent, oxField::T_RAW);
-            }
+            $oRatePay = oxNew('fcporatepay');
+            $sProfileId = $this->_oFcpoHelper->fcpoGetSessionVariable('ratepayprofileid');
+            $aProfileData = $oRatePay->fcpoGetProfileData($sProfileId);
+            $sRatePayShopId = $aProfileData['shopid'];
+            $this->oxorder__fcpoprofileident = new oxField($sRatePayShopId, oxField::T_RAW);
         }
     }
 
