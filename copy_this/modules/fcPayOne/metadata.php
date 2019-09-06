39f56cd6469c529381168a0f42dee25e29726b53
tkuchel <tkuchel@patsy.pui.payone-office.de>
2019-09-06 14:31:29 +0200

    version bump to 233

--- copy_this/modules/fcPayOne/metadata.php
+++ copy_this/modules/fcPayOne/metadata.php
@@ -34,7 +34,7 @@ $aModule = array(
                             https://www.payone.de
                         </a>',
     'thumbnail'     => 'picture.gif',
-    'version'       => '2.3.1',
+    'version'       => '2.3.3',
     'author'        => 'FATCHIP GmbH',
     'email'         => 'kontakt@fatchip.de',
     'url'           => 'https://wiki.fatchip.de/public/faqpayone',
