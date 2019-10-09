f7572e950db080a403e5702081d35ef2d560e8bd
andrefatchip <andre.herrmann@fatchip.de>
2019-10-09 14:42:50 +0200

    OXID-294: Added missing abbreviations

--- copy_this/modules/fcPayOne/application/models/fcpoconfigexport.php
+++ copy_this/modules/fcPayOne/application/models/fcpoconfigexport.php
@@ -492,6 +492,12 @@ class fcpoconfigexport extends oxBase {
             'fcpoklarna' => 'fnc',
             'fcpobarzahlen' => 'csh',
             'fcpopaydirekt' => 'wlt',
+            'fcpopo_bill' => 'fnc',
+            'fcpopo_debitnote' => 'fnc',
+            'fcpopo_installment' => 'fnc',
+            'fcporp_bill' => 'fnc',
+            'fcpocreditcard_iframe' => 'cc',
+            'fcpobillsafe' => 'fnc',
         );
 
         if (isset($aAbbreviations[$sPaymentId])) {
