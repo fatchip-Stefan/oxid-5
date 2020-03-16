28b4351b52720b2560058def89671d2602aa505e
andrefatchip <andre.herrmann@fatchip.de>
2020-03-16 13:22:22 +0100

    OXID-305: Removed cc type discover

--- copy_this/modules/fcPayOne/extend/application/controllers/fcPayOnePaymentView.php
+++ copy_this/modules/fcPayOne/extend/application/controllers/fcPayOnePaymentView.php
@@ -372,7 +372,6 @@ class fcPayOnePaymentView extends fcPayOnePaymentView_parent {
                 $this->getJCB(),
                 $this->getMaestroInternational(),
                 $this->getMaestroUK(),
-                $this->getDiscover(),
                 $this->getCarteBleue(),
             ),
             'sb' => array(
@@ -453,15 +452,6 @@ class fcPayOnePaymentView extends fcPayOnePaymentView_parent {
      */
     public function getMaestroUK() {
         return ($this->getConfigParam('blFCPOMaestroUKActivated') && $this->isPaymentMethodAvailableToUser('U', 'cc'));
-    }
-
-    /**
-     * Check if sub payment method Discover is available to the user
-     * 
-     * @return bool
-     */
-    public function getDiscover() {
-        return ($this->getConfigParam('blFCPODiscoverActivated') && $this->isPaymentMethodAvailableToUser('C', 'cc'));
     }
 
     /**
@@ -643,7 +633,6 @@ class fcPayOnePaymentView extends fcPayOnePaymentView_parent {
         $this->_fcpoSetCCMetaData($oPayment, 'J', 'JCB');
         $this->_fcpoSetCCMetaData($oPayment, 'O', 'Maestro International');
         $this->_fcpoSetCCMetaData($oPayment, 'U', 'Maestro UK');
-        $this->_fcpoSetCCMetaData($oPayment, 'C', 'Discover');
         $this->_fcpoSetCCMetaData($oPayment, 'B', 'Carte Bleue');
 
         return $this->_aPaymentCCMetaData;
@@ -1125,7 +1114,6 @@ class fcPayOnePaymentView extends fcPayOnePaymentView_parent {
             'J' => $this->getJCB(),
             'O' => $this->getMaestroInternational(),
             'U' => $this->getMaestroUK(),
-            'C' => $this->getDiscover(),
             'B' => $this->getCarteBleue(),
         );
 
